cb17194a77f3f807e3ae5ad50244d8cbaa32f5a6
James Souter <james.souter@diamond.ac.uk>
2023-11-17 09:15:27 +0000

    use new updateTimeStamps method

--- App/src/QImaging.cpp
+++ App/src/QImaging.cpp
@@ -472,9 +472,8 @@ void QImage::consumerTask()
 			if ((arrayCallbacks))
 			{
 				// Put the frame number and time stamp into the buffer
-				updateTimeStamp(&ndArray->epicsTS);
 				pFrames[frameId]->ndArray->uniqueId = frameId;
-				pFrames[frameId]->ndArray->timeStamp = ndArray->epicsTS.secPastEpoch + ndArray->epicsTS.nsec / 1.e9;
+				updateTimeStamps(pFrames[frameId]->ndArray);
 				// Get any attributes that have been defined for this driver
 				this->getAttributes(pFrames[frameId]->ndArray->pAttributeList);
 				// Call the NDArray callback
